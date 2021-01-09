72f0492fbfa6d663ff41b463388bbd5d8cc025ca
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2021-01-09 18:32:34 +0100

    Add kmm-sample

---
+++ samples/cache4k-kmm/settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+rootProject.name = "cache4k"
+
+include(":androidApp")
+include(":shared")
---
+++ samples/cache4k-kmm/shared/src/commonMain/kotlin/com/javiersc/cache4k/shared/User.kt
@@ -0,0 +1,4 @@
+
+package com.javiersc.cache4k.shared
+
+data class User(val id: Int, val name: String)
