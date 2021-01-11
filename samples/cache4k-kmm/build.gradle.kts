49a670ce5b4c601023a45c62c272a570b35850dd
Yang <reactivecircus@gmail.com>
2021-01-11 16:50:42 +1100

    Update build configs and add detekt for kmm sample.

--- samples/cache4k-kmm/build.gradle.kts
+++ samples/cache4k-kmm/build.gradle.kts
@@ -1,23 +1,29 @@
-buildscript {
-    repositories {
-        gradlePluginPortal()
-        jcenter()
-        google()
-        mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
-        classpath("com.android.tools.build:gradle:4.0.2")
-        classpath("org.jetbrains.compose:compose-gradle-plugin:0.3.0-build139")
-    }
+plugins {
+    kotlin("multiplatform") version "1.4.21" apply false
+    id("com.android.application") version "4.0.2" apply false
+    id("org.jetbrains.compose") version "0.3.0-build139" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.15.0"
 }
 
 allprojects {
     repositories {
+        mavenCentral()
         google()
         jcenter()
-        mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    detekt {
+        input = files("src/")
+        failFast = true
+        config = files("${project.rootDir}/detekt.yml")
+        buildUponDefaultConfig = true
+        reports {
+            html.destination = file("${project.buildDir}/reports/detekt/${project.name}.html")
+        }
+    }
+    dependencies.add("detektPlugins", "io.gitlab.arturbosch.detekt:detekt-formatting:1.15.0")
 }
