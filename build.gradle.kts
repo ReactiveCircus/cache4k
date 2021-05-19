021ac3ac087e5b151c5a0ba7b1a7d0db957f754f
Yang <reactivecircus@gmail.com>
2021-05-20 02:56:52 +1000

    Dokka 1.4.32.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ val detektVersion = "1.17.0"
 plugins {
     kotlin("multiplatform") version "1.5.0" apply false
     id("com.vanniktech.maven.publish") version "0.15.1" apply false
-    id("org.jetbrains.dokka") version "1.4.10.2"
+    id("org.jetbrains.dokka") version "1.4.32"
     id("io.gitlab.arturbosch.detekt") version "1.17.0"
     id("binary-compatibility-validator") version "0.4.0" apply false
 }
