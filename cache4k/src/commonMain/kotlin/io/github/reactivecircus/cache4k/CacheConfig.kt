d13a8e46228034cb33901c1290994e51a79595d9
lcamilo <lhenriquegomescamilo@gmail.com>
2022-01-03 21:15:47 +0000

    :recycle: Using the inline functions.

--- cache4k/src/commonMain/kotlin/io/github/reactivecircus/cache4k/CacheConfig.kt
+++ cache4k/src/commonMain/kotlin/io/github/reactivecircus/cache4k/CacheConfig.kt
@@ -1,9 +1,9 @@
 package io.github.reactivecircus.cache4k
 
-public fun <K : Any, V : Any> buildCache(init: Cache.Builder.() -> Unit): Cache<K, V> {
+public inline fun <K : Any, V : Any> buildCache(init: Cache.Builder.() -> Unit): Cache<K, V> {
     val cacheBuilder = Cache.Builder()
     cacheBuilder.init()
     return cacheBuilder.build()
 }
 
-public fun <K : Any, V : Any> buildDefaultCache(): Cache<K, V> = buildCache {}
+public inline fun <K : Any, V : Any> buildDefaultCache(): Cache<K, V> = buildCache {}
