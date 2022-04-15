dc0ab316c9252f09032e657ae50fe477dfd98f37
David Chavez <david@dcvz.io>
2022-04-16 01:19:59 +0200

    Make mutable set extensions internal

--- cache4k/src/commonMain/kotlin/io/github/reactivecircus/cache4k/MutableSetExt.kt
+++ cache4k/src/commonMain/kotlin/io/github/reactivecircus/cache4k/MutableSetExt.kt
@@ -4,7 +4,7 @@ package io.github.reactivecircus.cache4k
  * Adds element and reorders insertion order to have inserted element be placed last,
  * regardless of whether the element was already in the set.
  */
-public fun<T> MutableSet<T>.addLastOrReorder(element: T): Boolean {
+internal fun<T> MutableSet<T>.addLastOrReorder(element: T): Boolean {
     val exists = remove(element)
     add(element)
 
