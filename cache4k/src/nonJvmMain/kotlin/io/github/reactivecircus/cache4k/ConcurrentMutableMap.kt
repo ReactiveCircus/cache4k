d0f9f74d476f6dc216a21e99a7ee64963a6d2265
Yang <ychescale9@gmail.com>
2023-04-30 14:29:36 +1000

    Simplify `actual` implementations.

--- cache4k/src/nonJvmMain/kotlin/io/github/reactivecircus/cache4k/ConcurrentMutableMap.kt
+++ cache4k/src/nonJvmMain/kotlin/io/github/reactivecircus/cache4k/ConcurrentMutableMap.kt
@@ -5,25 +5,15 @@ import co.touchlab.stately.collections.IsoMutableMap
 internal actual class ConcurrentMutableMap<Key : Any, Value : Any> {
     private val map = IsoMutableMap<Key, Value>()
 
-    actual val size: Int
-        get() = map.size
+    actual val size: Int get() = map.size
 
-    actual val values: Collection<Value>
-        get() = map.values
+    actual val values: Collection<Value> get() = map.values
 
-    actual operator fun get(key: Key): Value? {
-        return map[key]
-    }
+    actual operator fun get(key: Key): Value? = map[key]
 
-    actual fun put(key: Key, value: Value): Value? {
-        return map.put(key, value)
-    }
+    actual fun put(key: Key, value: Value): Value? = map.put(key, value)
 
-    actual fun remove(key: Key): Value? {
-        return map.remove(key)
-    }
+    actual fun remove(key: Key): Value? = map.remove(key)
 
-    actual fun clear() {
-        map.clear()
-    }
+    actual fun clear() = map.clear()
 }
